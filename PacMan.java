0b02d37cb4a0c99610c69af7a61809e33f76a3f8
Leerng <39564803+Leerng@users.noreply.github.com>
2018-05-24 09:00:42 -0700

    Create PacMan.java

---
+++ PacMan.java
@@ -0,0 +1,36 @@
+public class PacMan extends JPanel {
+  private static final long serialVersionUID = 1L;
+  private ScorePanel scorePanel = new ScorePanel(0, Color.YELLOW);
+  
+  public static void main(String[] args) throws Exception{
+      String className = UIManager.getCrossPlatformLookAndFeelClassName();
+      UIManager.setLookAndFeel(className); 
+      SwingUtilities.invokeLater(new Runnable() {
+         public void run() {
+            new PacMan();
+         }
+      });
+   }
+   
+   public PacMan() {
+      initGUI();
+      setTitle("PacMan");
+      setResizable(false);
+      pack();
+      setLocationRelativeTo(null);
+      setVisible(true);
+      setDefaultCloseOperation(EXIT_ON_CLOSE);
+   }
+   
+   private void initGUI() {
+      JLabel titleLabel = new JLabel("PacMan");
+      add(titleLabel, BorderLayout.PAGE_START);
+      //mainPanel
+      JPanel mainPanel = new JPanel();
+      mainPanel.setBackground(Color.GREEN);
+      mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.Y_AXIS));
+      add(mainPanel, BorderLayout.CENTER);
+      //scorePanel
+      mainPanel.add(scorePanel);
+   }
+}
