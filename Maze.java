383e33602da5fd5bff077547243cf7203995caf4
Leerng <39564803+Leerng@users.noreply.github.com>
2018-05-24 09:14:40 -0700

    Update Maze.java

--- Maze.java
+++ Maze.java
@@ -1,16 +1,16 @@
 public class Maze {
-  public static final int CELL_SIZE = 30;
-  public static final String FILE_NAME = "/maze.txt"
-  private static final int HALF_CELL_SIZE = CELL_SIZE / 2;
-  private static final int QUARTER_CELL_SIZE = CELL_SIZE / 4;
-  private static final int TYPE_PATH = 0;
-  private static final int TYPE_WALL = 1;
-  private static final int TYPE_CHEESE = 2;
-  private int rows;
-  private int columns;
-  private ArrayList<String> mazeData = Maze();
+    public static final int CELL_SIZE = 30;
+    public static final String FILE_NAME = "/maze.txt"
+    private static final int HALF_CELL_SIZE = CELL_SIZE / 2;
+    private static final int QUARTER_CELL_SIZE = CELL_SIZE / 4;
+    private static final int TYPE_PATH = 0;
+    private static final int TYPE_WALL = 1;
+    private static final int TYPE_CHEESE = 2;
+    private int rows;
+    private int columns;
+    private ArrayList<String> mazeData = Maze();
   
-  public Maze() {
-    readMazeData();
-  }
+    public Maze() {
+        readMazeData();
+    }
 }
