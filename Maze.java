00b21525e903db79db1e506f3f2f9d75fa365a5a
Carly-Guzylak <39564136+Carly-Guzylak@users.noreply.github.com>
2018-05-31 09:13:18 -0700

    Update Maze.java
    ended on pg 323

--- Maze.java
+++ Maze.java
@@ -13,6 +13,17 @@ public class Maze {
     private int rows;
     private int columns;
     private ArrayList<String> mazeData = new ArrayList<String>();
+    private int [][]cell;
+    private int cheeseCount = 0;
+    private int mouseRow;
+    private int mouseCol;
+    private int catRow;
+    private int calCol;
+    private ArrayList<Integer> extraMiceX = new ArrayList<Integer>;
+    private ArrayList<Integer> extraMiceY = new ArrayList<Integer>;
+    //Note from book: Maze Data file has the capital letter X to represent walls,
+    // a space to represent a path with nothing in it, and a period to represent a path
+    // with a piece of cheese in it.
   
     public Maze() {
         readMazeData();
@@ -31,7 +42,26 @@ public class Maze {
             line = mazeData.get(0);
             columns = line.length();
             //end of page 318
-        }    
+            cell = new Array[rows][columns];
+            for(int row = 0; row <= rows; row++) {
+                line = mazeData.get(row);
+                for(int col = 0; col <= columns; col++) {
+                    char c = line.charAt(col);   
+                    switch (c) {
+                        case 1: c = 'X';    
+                            cell[row][col] = TYPE_WALL; 
+                            break;
+                        case 2: c = ' ';
+                            cell[row][col] = TYPE_PATH;
+                            break;
+                        case 3: c = '.';
+                            cell[row][col] = TYPE_CHEESE;
+                            cheeseCount++;
+                            breakl;
+                    }//switch
+                }//2nd for
+            }//1st for
+        }//try    
         catch(NullPointerException e) {
             String message = "File not found.";
             JOptionPane.showMessageDialog(null, message);
