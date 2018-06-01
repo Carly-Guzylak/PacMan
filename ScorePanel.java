227c9e4b1492a3ebc8a04b3a71b98c9fb9612189
Leerng <39564803+Leerng@users.noreply.github.com>
2018-06-01 09:31:48 -0700

    Add files via upload

---
+++ ScorePanel.java
@@ -0,0 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ScorePanel extends JPanel {
+   private String score;
+   private JLabel scoreLabel = new JLabel();
+   
+   public ScorePanel(int score, Color color) {
+      super();
+      this.score = "" + score;
+      setText(this.score);
+      setBackground(color);
+      
+   }
+   
+}
